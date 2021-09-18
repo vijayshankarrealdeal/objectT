772ef3cde5e3d6e63547f9d584cfb42a474ae596
Yashraj Singh <yyyyashssssingh@gmail.com>
2021-09-18 17:00:34 +0530

    ..

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Test"),),
+      body: Center(child: RaisedButton(onPressed: (){
+        _optionsDialogBox();
+      }, child: Text("Button"),),),
+    );
+  }
+
+  openCamera() async {
+    var picture = await ImagePicker.platform.pickImage(
+      source: ImageSource.camera,
+    );
+  }
+
+  openGallery() async {
+    var gallery = await ImagePicker.platform.pickImage(
+      source: ImageSource.gallery,
+    );
+  }
+
+  _optionsDialogBox() {
+    return showDialog(context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: new SingleChildScrollView(
+              child: new ListBody(
+                children: <Widget>[
+                  GestureDetector(
+                    child: new Text('Take a picture'),
+                    onTap: openCamera,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                  ),
+                  GestureDetector(
+                    child: new Text('Select from gallery'),
+                    onTap: openGallery,
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+}
